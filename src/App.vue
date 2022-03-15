c4749c9279d67f70bc762283e5c25d5a83ac518f
Syifa Ainnur <arwahitem01@gmail.com>
2022-03-15 17:13:35 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,30 @@
 export default {
   data() {
     return {
-      currentTask: "",
-      data: [],
+      isEditing: false,
+      ToDo: "",
+      ToDos: [],
+      selectedTodo: null,
     };
   },
   methods: {
     submitTodo() {
-      if (!this.currentTask.length) {
-        return alert("Your Input is Empty");
-      }
-      this.data.push(this.currentTask);
-      this.currentTask = "";
-      console.log(this.data);
+      this.ToDos.push(this.ToDo);
+      this.ToDo = "";
     },
     removeTodo(index) {
       console.log(index);
-      this.data.splice(index, 1);
+      this.ToDos.splice(index, 1);
+    },
+    updateTodo() {
+      this.ToDos.splice(this.selectedIndex, 1, this.ToDo);
+      this.ToDo = "";
+      this.isEditing = false;
+    },
+    editTodo(index, ToDo) {
+      this.isEditing = true;
+      this.ToDo = ToDo;
+      this.selectedIndex = index;
     },
   },
 };
@@ -25,48 +33,48 @@ export default {
 
 <template>
   <div class="container mt-5">
+    <h1 class="display-2 d-flex justify-content-center">ToDo List</h1>
     <div class="row">
       <div class="col-5 m-auto">
-        <div class="row">
-          <div class="col-12">
-            <form @submit.prevent="submitTodo">
-              <div class="mb-3">
-                <label for="inputTodo" class="form-label fw-bold text-light">Input To Do</label>
-                <input
-                  autocomplete="off"
-                  type="text"
-                  class="form-control"
-                  v-model="currentTask"
-                  placeholder="Enter your To Do"
-                  id="inputTodo"
-                />
-              </div>
-              <button class="btn btn-primary btn-block" type="submit">
-                Add To Do
-              </button>
-            </form>
-          </div>
-          <div class="col-12 mt-5 text-light">
-            task kamu : {{ currentTask }}
-          </div>
-          <div class="mt-4">
-            <div class="card my-2" v-for="(toDo, index) in data" :key="index">
-              <div class="card-body row">
-                <div class="col-10 my-auto text-dark"> {{ toDo }}</div>
-                <div class="col-2 mt-auto">
-                    <button @click="removeTodo(index)" class="btn btn-dark ms-auto">X</button>
-                  </div>
-              </div>
-            </div>
-          </div>
+        <div v-if="!isEditing" >
+          <input
+            type="text"
+            class="form-control mt-2 mb-2"
+            v-model="ToDo"
+            placeholder="Masukkan ToDo"
+            id="inputTodo"
+          />
+
+          <button class="btn btn-primary btn-block" type="submit" value="add" @click="submitTodo">
+            simpan
+          </button>
         </div>
+        <div v-else>
+          <input type="text" class="form-control mt-2 mb-2" v-model="ToDo"/>
+          <button class="btn btn-primary btn-block" type="submit" value="update" @click="updateTodo">
+            Update
+          </button>
+        </div>
+
+        <ol>
+          <li v-for="(ToDo, index) in ToDos" class="mb-2">
+            {{ ToDo }}
+            <button class="btn btn-secondary mr-1 btn-space" @click="editTodo(index, ToDo)">
+              Edit
+            </button>
+            <button class="btn btn-danger" @click="removeTodo(index)">
+              Delete
+              </button>
+          </li>
+        </ol>
+
       </div>
     </div>
   </div>
 </template>
 
 <style>
-body {
-  background-color: #171717;
+.btn-space {
+  margin-right: 10px;
 }
 </style>
